82b981c22ba2aca831115727a8b119ee0952086d
marcobussi <sargon05@hotmail.com>
2025-08-03 16:07:26 +0200

    fix

--- DMFlowManager-entities/src/main/java/it/dmsoft/flowmanager/be/repositories/FlowLogDetailsRepository.java
+++ DMFlowManager-entities/src/main/java/it/dmsoft/flowmanager/be/repositories/FlowLogDetailsRepository.java
@@ -1,11 +1,10 @@
 package it.dmsoft.flowmanager.be.repositories;
 
-import java.math.BigDecimal;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import it.dmsoft.flowmanager.be.entities.FlowLogDetails;
+import it.dmsoft.flowmanager.be.keys.FlowLogDetailsId;
 
 @Repository
-public interface FlowLogDetailsRepository extends JpaRepository<FlowLogDetails, BigDecimal> { }
+public interface FlowLogDetailsRepository extends JpaRepository<FlowLogDetails, FlowLogDetailsId> { }
